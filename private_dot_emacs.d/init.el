9aca86edfbbe2b9884724dfdff1bfdcae2b72aba
Suslik <dunordavind@protonmail.com>
2023-02-26 12:02:13 +0100

    Migrated to chezmoi

---
+++ private_dot_emacs.d/init.el
@@ -0,0 +1,20 @@
+(require 'package)
+(package-initialize)
+
+
+;; ensure custom does not modify this file
+(setq custom-file "~/.emacs-custom.el")
+(load custom-file 'noerror)
+
+;; get rid of the 'editing softlink' notification
+(setq find-file-visit-truename t)
+
+;; load package repositories
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+(package-refresh-contents)
+
+;; load the actual config
+(require 'org)
+(org-babel-load-file
+ (expand-file-name "config/init.org" user-emacs-directory))

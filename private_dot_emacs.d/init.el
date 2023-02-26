7022b216431147bad54e353b8af04331952d0a45
Suslik <dunordavind@protonmail.com>
2023-02-26 15:02:57 +0000

    moved package initialisation to org config

--- private_dot_emacs.d/init.el
+++ private_dot_emacs.d/init.el
@@ -1,17 +1,7 @@
-(require 'package)
-(package-initialize)
-
-
 ;; ensure custom does not modify this file
 (setq custom-file "~/.emacs-custom.el")
 (load custom-file 'noerror)
 
-;; load package repositories
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
-(package-refresh-contents)
-
-;; load the actual config
 (require 'org)
 (org-babel-load-file
  (expand-file-name "config/init.org" user-emacs-directory))
